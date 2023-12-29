0cfee87497c2f15a51dbf219b3f5d382372d57cf
Mika PS <mika>
2023-12-29 10:08:32 -0800

    uploading files

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, make_response, jsonify
+from WebScraping import handle_csv
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/call_function', methods=['GET'])
+def call_function():
+    hasHeaders = int(request.args.get('hasHeaders'))
+    appliance_type = request.args.get('appliance')
+    count_products = int(request.args.get('countproducts'))
+    url = request.args.get('url')
+    if (url[0:8] != "https://"): 
+        return make_response(jsonify(["URLError", "URLError"]))
+    data, result = handle_csv(url, appliance_type, count_products, True, hasHeaders)
+    # save values to a file
+    response = make_response(jsonify([data, result]))
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True)
